fef6e024b3ec8e719448af32ada0131b5dfab2b0
SMKrizan <smkrizan@gmail.com>
2020-12-05 16:19:32 -0600

    homepage complete

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,17 +4,46 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     // now that we are using a template engine, we can use "render" rather than "res.send()" or "res.sendFile()" for the response. Here we are asking for the "homepage.handlebars" template to be rendered. Handlebars will feed this 'homepage' template into the main.handlebars template and respond with a complete html file.
-    res.render('homepage', {
-        id: 1,
-        post_url: "https://handlebarsjs.com/guide/",
-        title: "Handlebars Docs",
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: "test_user"
-        }
-    });
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // passes a single post object into the homepage template; using 'model.get({ plain: true })' method in order to serialize each Sequelize object, that is, limit the response to include only the specified attributes rather than the extensive Sequelize object. Use of the '.map()' method loops through each Sequelize object and then saves the results to a new 'posts' array
+            console.log('dbPostData[0]: ', dbPostData[0]);
+            console.log('dbPostData[0].get({ plain: true }): ', dbPostData[0].get({ plain: true }))
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;

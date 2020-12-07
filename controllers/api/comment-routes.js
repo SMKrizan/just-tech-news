bae5eda1ba395104fd7e55c2434fbcc168a0fd65
SMKrizan <smkrizan@gmail.com>
2020-12-07 17:02:33 -0600

    comments now posting

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,11 +7,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    console.log('req.session: ', req.session);
+    console.log('req.body: ', req.body);
+
     if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
-            user_id: req.body.user_id,
-            post_id: req.body.post_id
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
         })
             .then(dbCommentData => res.json(dbCommentData))
             .catch(err => {

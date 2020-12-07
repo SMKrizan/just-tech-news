7c2608f898bb0ec57226a0201d6169b9f022528a
SMKrizan <smkrizan@gmail.com>
2020-12-07 15:13:59 -0600

    working on comment form

---
+++ public/javascript/upvote.js
@@ -0,0 +1,25 @@
+// defining click handler as an async function because it makes an async PUT request with fetch()
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
